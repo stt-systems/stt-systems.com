86fad258bccbc8d77c8f1496283bcf44e1f4e3cf
cbuchart <cbuchart@stt-systems.com>
2018-07-10 14:40:48 +0200

    CSS sources moved to src directory

--- css/src/build.bat
+++ css/src/build.bat
@@ -7,7 +7,7 @@ if not "%1"=="" set style=%1
 
 for %%F in (*.scss) do (
   echo sass %%F...
-  call sass %%F %%~nF.min.css --no-source-map --style=%STYLE%
+  call sass %%F ..\%%~nF.min.css --no-source-map --style=%STYLE%
 )
 
 exit /b 0
