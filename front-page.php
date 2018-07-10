514d88033cd59fd18274f8609493e0c31b23d5d5
cbuchart <cbuchart@stt-systems.com>
2018-07-10 16:34:49 +0200

    Full screen land page

--- front-page.php
+++ front-page.php
@@ -6,26 +6,30 @@ get_header();
 <link rel="stylesheet" property="stylesheet" href="<?php echo my_get_url_for_path(WL_TEMPLATE_LOCAL_DIR . '/css/front-page.min.css'); ?>" />
 
 <div style="height: 100vh">
-	<div class="row" style="display: table">
+	<div class="row" style="display: table; height: 100%">
     <div class="col-md-6 col-sm-6 panel">
-      <div class="cold-md-3 col-sm-3"></div>
-      <div class="col-md-6 col-sm-6">
+      <a style="display: block" href="<?php echo get_page_permalink("capture"); ?>">
+      <div class="col-md-2 col-sm-2"></div>
+      <div class="col-md-8 col-sm-8">
         <div class="content-box">
           <h1>Motion Analysis Solutions</h1>
           <h3>Premium technologies for human motion studies. Ready to use by scientists, sports scientists and clinitians</h3>
         </div>
       </div>
-      <div class="cold-md-3 col-sm-3"></div>
+      <div class="col-md-2 col-sm-2"></div>
+      </a>
     </div>
     <div class="col-md-6 col-sm-6 panel">
-      <div class="cold-md-3 col-sm-3"></div>
-      <div class="col-md-6 col-sm-6">
+      <a style="display: block" href="<?php echo get_page_permalink("industry"); ?>">
+      <div class="col-md-2 col-sm-2"></div>
+      <div class="col-md-8 col-sm-8">
         <div class="content-box">
           <h1>Industry 4.0</h1>
           <h3>Turn-key solutions for companies seeking smart and automated manufacturing &amp; monitoring processes</h3>
         </div>
       </div>
-      <div class="cold-md-3 col-sm-3"></div>
+      <div class="col-md-2 col-sm-2"></div>
+      </a>
     </div>
   </div>
 </div>
