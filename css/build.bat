85c5e66ad5274890241be03e3bcc8de994f53c3c
cbuchart <cbuchart@stt-systems.com>
2018-07-10 14:39:05 +0200

    sass tool now required to be installed globally (npm)

--- css/build.bat
+++ css/build.bat
@@ -5,6 +5,9 @@ echo Generating CSS...
 set STYLE=compressed
 if not "%1"=="" set style=%1
 
-for %%F in (*.scss) do call c:\compiler\3rdParty\sass\sass.bat %%F %%~nF.min.css --no-source-map --style=%STYLE%
+for %%F in (*.scss) do (
+  echo sass %%F...
+  call sass %%F %%~nF.min.css --no-source-map --style=%STYLE%
+)
 
 exit /b 0
