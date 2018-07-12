a1f830f4ee0b9c152de28cf6e51f3f3de5b1a334
cbuchart <cbuchart@stt-systems.com>
2018-07-12 16:05:03 +0200

    CSS build script now excludes files starting with an underscore
    It also cleans the output folder before start

--- css/src/build.bat
+++ css/src/build.bat
@@ -5,9 +5,16 @@ echo Generating CSS...
 set STYLE=compressed
 if not "%1"=="" set STYLE=%1
 
-for %%F in (*.scss) do (
-  echo sass %%F...
-  call sass %%F ..\%%~nF.min.css --no-source-map --style=%STYLE%
-)
+del ..\*.css 2> nul
+for %%F in (*.scss) do call :sass %%F
 
 exit /b 0
+
+:sass
+set FILENAME=%1
+if not "%FILENAME:~0,1%"=="_" (
+  echo sass %1...
+  call sass %1 ..\%~n1.min.css --no-source-map --style=%STYLE%
+)
+
+exit /b
