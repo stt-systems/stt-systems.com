e0cf7c21951d5ef4e95aae2d047e80eba1ca808b
cbuchart <cbuchart@stt-systems.com>
2018-07-19 14:48:14 +0200

    Posts are alternatively left and right aligned in blog page

--- content.php
+++ content.php
@@ -7,7 +7,12 @@ if (empty($page_style_array)) {
 }
 $page_style = "style-$style_name";
 if (!is_page()) {
-	if (!is_single()) { ?>
+	if (!is_single()) {
+    if ($wp_query->current_post % 2 == 0) { ?>
+      <div class="col-md-3 col-sm-3">
+      </div><?php
+    }?>
+    <div class="col-md-9 col-sm-9">
 		<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_title(); ?></a></h2><?php
 	} else { ?>
 		<div class="space-sep20"></div><?php
@@ -18,13 +23,13 @@ if (!is_page()) {
 				<span class="fa fa-calendar"></span><?php the_time('M j, Y'); ?>
 			</div><?php
 			
-			if(get_the_category_list() != '' and false) { ?>
+			if (get_the_category_list() != '' and false) { ?>
 				<div class="blog-post-details-item blog-post-details-item-left">
 					<span class="fa fa-folder-open"></span><?php the_category(', ', 'single', get_the_ID()); ?>
 				</div><?php
 			}
 			
-			if(get_the_tag_list() != '') { ?>
+			if (get_the_tag_list() != '') { ?>
 				<div class="blog-post-details-item blog-post-details-item-left">
 					<span class="fa fa-tags"></span><?php the_tags('', ', ', '<br />'); ?>
 				</div><?php
@@ -32,13 +37,23 @@ if (!is_page()) {
 		</div><?php
 	}
 }
+
 if ($post->post_type == 'page') {
 	add_page_path();
 } ?>
 <div class="space-sep20"></div>
 <div class="blog-post-body <?php echo "$page_style";?>"><?php
-	echo stt_replace_content(get_the_content());
-	if ($post->post_type == 'post') {
-		add_share_buttons();
-	} ?>
+  echo stt_replace_content(get_the_content());
+  if ($post->post_type == 'post') {
+    add_share_buttons();
+  } ?>
 </div>
+
+<?php
+if (!is_page() and !is_single()) { ?>
+  </div><?php
+  if ($wp_query->current_post % 2 == 1) { ?>
+    <div class="col-md-3 col-sm-3">
+    </div><?php
+  }
+}?>
