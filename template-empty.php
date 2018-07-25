93ad140e3c172eb70de70050da587af0d308c06c
cbuchart <cbuchart@stt-systems.com>
2018-07-25 11:08:25 +0200

    Added rounded corner to some buttons

--- template-empty.php
+++ template-empty.php
@@ -6,7 +6,7 @@ get_header(); ?>
 			<div class="not-found-404">
 				<h2><?php echo ':('; ?><i class="iscon-remove-sign skin-text"></i></h2>
 				<p><?php echo "We're sorry, but the page you were looking for doesn't have any content."; ?></p> 
-				<p class="search-404"><a href="<?php echo esc_html(site_url()); ?>" class="btn btn-primary search-submit"><?php echo 'Go to Homepage'; ?></a></p>
+				<p class="search-404"><a href="<?php echo esc_html(site_url()); ?>" class="btn btn-primary"><?php echo 'Go to Homepage'; ?></a></p>
 			</div>
 		</div>
 	</div>
