7021635f0f023912eb0e2a6fd0f3b254312ed770
cbuchart <cbuchart@stt-systems.com>
2018-07-26 11:25:50 +0200

    Added method to get current URL

--- includes/globals.php
+++ includes/globals.php
@@ -4,6 +4,10 @@ function is_page_valid() {
   return true;
 }
 
+function get_current_url() {
+	return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+}
+
 function get_top_level_slug() {
   if (is_page_valid() === false) return 'capture';
   
