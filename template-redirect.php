f73875400ad762b6865f944c2d70c96b4ddd3cfb
cbuchart <cbuchart@stt-systems.com>
2018-07-26 11:25:29 +0200

    Fixed typo in comment

--- template-redirect.php
+++ template-redirect.php
@@ -1,5 +1,5 @@
 <?php //Template Name:Redirect
 $new_url = get_post_meta($post->ID, 'url', true);
-header('Location: ' . get_site_url() . $new_url); // mepty URLs will redirect to the homepage
+header('Location: ' . get_site_url() . $new_url); // empty URLs will redirect to the homepage
 exit;
 ?>
