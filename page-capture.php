53f232d35bac84be2f94e50c52798e730d265a9c
cbuchart <cbuchart@stt-systems.com>
2018-09-05 10:34:32 +0200

    Removed unnecessary HTML code

--- page-capture.php
+++ page-capture.php
@@ -4,7 +4,7 @@ get_header();
 wp_enqueue_style('front-page', get_template_directory_uri() . '/css/front-page.min.css');
 ?>
 
-<div class="row" style="display: table; height: 80vh; width: 100%; margin: 0; padding: 0">
+<div class="row" style="display: table; height: 100vh; width: 100%; margin: 0">
   <div class="col-md-4 col-sm-4 panel">
     <div class="col-md-2 col-sm-2"></div>
     <div class="col-md-8 col-sm-8">
@@ -63,8 +63,8 @@ wp_enqueue_style('front-page', get_template_directory_uri() . '/css/front-page.m
 <?php
 $clients = get_files_in_dir('clients/capture');
 $n = count($clients); ?>
-<div class="container-fluid boxshadow">
-<div id="clientsCarousel" class="carousel slide carousel-fade" data-ride="carousel" style="margin-bottom: 0px">
+<div class="container-fluid">
+<div id="clientsCarousel" class="carousel slide carousel-fade" data-ride="carousel" style="margin-bottom: 0">
   <div class="carousel-inner" style="background: #565656;"><?php
   $i = 0;
   foreach ($clients as $client) {
