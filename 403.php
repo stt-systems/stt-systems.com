d21f5a8a4813595ddf2d71d9f1c40686fdfe224d
cbuchart <cbuchart@stt-systems.com>
2018-09-11 12:25:44 +0200

    Minor style renaming

--- 403.php
+++ 403.php
@@ -2,7 +2,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12 col-sm-12">
-			<div class="not-found-404">
+			<div class="not-found-40x">
 				<h2><?php echo 'Error 403'; ?><i class="iscon-remove-sign skin-text"></i></h2>
 				<p><?php echo "We're sorry, you don't have access to this file."; ?></p> 
 				<p class="search-404"><a href="<?php echo esc_html(site_url()); ?>" class="btn btn-primary search-submit"><?php echo 'Go to Homepage'; ?></a></p>					
--- template-empty.php
+++ template-empty.php
@@ -3,7 +3,7 @@ get_header(); ?>
 <div class="container">
 	<div class="row">
 		<div class="col-md-12 col-sm-12">			
-			<div class="not-found-404">
+			<div class="not-found-40x">
 				<h2><?php echo ':('; ?><i class="iscon-remove-sign skin-text"></i></h2>
 				<p><?php echo "We're sorry, but the page you were looking for doesn't have any content."; ?></p> 
 				<p class="search-404"><a href="<?php echo esc_html(site_url()); ?>" class="btn btn-primary"><?php echo 'Go to Homepage'; ?></a></p>
