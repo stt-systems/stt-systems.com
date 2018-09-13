7f2ba7edefa867789e88899cf8a077ff3528c21f
cbuchart <cbuchart@stt-systems.com>
2018-09-13 15:54:07 +0200

    Minor improvements to auto-update page

--- page-update.php
+++ page-update.php
@@ -1,16 +1,20 @@
 <?php
 if (!is_user_logged_in() || wp_get_current_user()->user_login != 'sttadmin') {
-  require('template-empty.php');
+  require('404.php');
   exit;
 }
 
 the_post();
 $content = get_the_content();
-echo 'Executing: <br>';
+echo "Executing: <br>";
 get_template_part('content');
 $old_path = getcwd();
 chdir(get_template_directory());
-echo '<br>Results: <br>';
+$cwd_path = getcwd();
+echo "<br>Old working directory: $old_path<br>";
+echo "<br>Current working directory: $cwd_path<br>";
+echo "<br>Results: <br>";
 echo shell_exec($content);
 chdir($old_path);
+echo "<br>End!<br>";
 ?>
