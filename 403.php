75e3e6548a2c51fd67a7466a19fb8e54b8db46fc
cbuchart <cbuchart@stt-systems.com>
2018-09-25 11:54:41 +0200

    Updated 404 and 403 pages

--- 403.php
+++ 403.php
@@ -5,11 +5,11 @@ echo get_page_top_spacer(); ?>
 	<div class="row">
 		<div class="col-md-12 col-sm-12">
 			<div class="not-found-40x">
-				<h2><?php echo 'Error 403'; ?><i class="iscon-remove-sign skin-text"></i></h2>
-				<p><?php echo "We're sorry, you don't have access to this file."; ?></p> 
+				<h2><i class="fas fa-fingerprint" style="font-size: 75%"></i> <?php echo 'Error 403'; ?> <i class="fas fa-fingerprint" style="font-size: 75%"></i></h2>
+				<p><?php echo __("We're sorry, you don't have access to this file."); ?></p> 
 				<p class="search-404"><a href="<?php echo esc_html(site_url()); ?>" class="btn btn-primary search-submit"><?php echo 'Go to Homepage'; ?></a></p>					
 			</div>
 		</div>
 	</div>
- </div>
+</div>
 <?php get_footer(); ?>
