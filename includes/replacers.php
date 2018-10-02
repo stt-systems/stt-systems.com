b6bd85249d406e794dece97f3553c57fc50ebf8f
cbuchart <cbuchart@stt-systems.com>
2018-10-02 12:58:13 +0200

    Minor code changes

--- includes/replacers.php
+++ includes/replacers.php
@@ -602,13 +602,13 @@ function replace_collapse_shortcode($atts, $content = null) {
 
 	$content = do_shortcode($content);
 
-	$class_a = 'class="collapsed"';
+	$class_a = ' class="collapsed"';
 	if ($collapsed == 'false') {
 		$class_a = '';
 		$class = "in $class";
 	}
 
-	$code = "<h3 class=\"collapse-header\"><a href=\"#$id\" $class_a data-toggle=\"collapse\">
+	$code = "<h3 class=\"collapse-header\"><a href=\"#$id\"$class_a data-toggle=\"collapse\">
   					<span class=\"if-collapsed\">&#9654; $title</span>
   					<span class=\"if-not-collapsed\">&#9660; $title</span>
 					</a></h3>";
