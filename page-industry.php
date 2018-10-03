40fa29b56c5403d55b0ea9e77d68f76ff939dd16
cbuchart <cbuchart@stt-systems.com>
2018-10-03 16:56:12 +0200

    Front-page and Industry fits tablet and mobile devices

--- page-industry.php
+++ page-industry.php
@@ -4,26 +4,26 @@ get_header();
 wp_enqueue_style('front-page', get_template_directory_uri() . '/css/front-page.min.css');
 ?>
 
-<div class="container-fluid front-page">
+<div class="container-fluid front-page area-industry">
 <div class="row">
-  <div class="col-md-6 col-sm-12 col-xs-12 panel">
-    <div class="col-md-2 col-sm-2"></div>
-    <div class="col-md-8 col-sm-8">
+  <div class="col-md-6 col-xs-12 panel">
+    <div class="col-md-2"></div>
+    <div class="col-md-8 col-sm-12">
       <a href="<?php echo get_page_permalink("machine-vision"); ?>">
-        <h1 style="margin-bottom: 221px">Machine vision</h1>
+        <h1>Machine vision</h1>
       </a>
     </div>
-    <div class="col-md-2 col-sm-2"></div>
+    <div class="col-md-2"></div>
     <div class="panel-bg" style="background-image:<?php echo css_darken_image("backgrounds/machine-vision.jpg", 0);?>"></div>
   </div>
-  <div class="col-md-6 col-sm-12 col-xs-12 panel">
-    <div class="col-md-2 col-sm-2"></div>
-    <div class="col-md-8 col-sm-8">
+  <div class="col-md-6 col-xs-12 panel">
+    <div class="col-md-2"></div>
+    <div class="col-md-8 col-sm-12">
       <a href="<?php echo get_page_permalink("product-configurators"); ?>">
-        <h1 style="margin-bottom: 221px">Product configurators</h1>
+        <h1>Product configurators</h1>
       </a>
     </div>
-    <div class="col-md-2 col-sm-2"></div>
+    <div class="col-md-2"></div>
     <div class="panel-bg" style="background-image:<?php echo css_darken_image("backgrounds/product-configurators.jpg", 0);?>"></div>
   </div>
 </div>
