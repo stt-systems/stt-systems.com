6e76b65f6f8f3bd4f8fc438ce6e29a3b065ec0f2
cbuchart <cbuchart@stt-systems.com>
2018-10-08 10:09:36 +0200

    Fixes problem with columns with different height

--- footer.php
+++ footer.php
@@ -128,6 +128,8 @@
 <script async src="<?php echo my_get_url_for_path(WL_TEMPLATE_LOCAL_DIR . '/js/jquery.scrollTo.min.js', false); ?>"></script>
 <script async src="<?php echo my_get_url_for_path(WL_TEMPLATE_LOCAL_DIR . '/js/jquery.unveil.min.js', false); ?>"></script>
 <script async src="<?php echo my_get_url_for_path(WL_TEMPLATE_LOCAL_DIR . '/js/links-index.min.js', false); ?>"></script>
+<script async src="//cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.0/jquery.matchHeight-min.js"></script>
+<script>$(document).ready(function(){$('.col-extra').matchHeight();});</script>
 
 <script src="//wurfl.io/wurfl.js"></script>
 <script>if(!WURFL.is_mobile){var s=document.createElement("script");s.src="<?php echo my_get_url_for_path(WL_TEMPLATE_LOCAL_DIR . '/js/retina.min.js', false); ?>",document.getElementsByTagName("body")[0].appendChild(s)}</script>
