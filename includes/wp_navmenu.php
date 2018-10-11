fcfb1e4998f618861ea45a8d2eb6ba5bf06308eb
cbuchart <cbuchart@stt-systems.com>
2018-10-11 16:14:26 +0200

    Removed unused menu code

--- includes/wp_navmenu.php
+++ includes/wp_navmenu.php
@@ -201,53 +201,6 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 
 		parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
 	}
-
-	/**
-	 * Menu Fallback
-	 * =============
-	 * If this function is assigned to the wp_nav_menu's fallback_cb variable
-	 * and a manu has not been assigned to the theme location in the WordPress
-	 * menu manager the function with display nothing to a non-logged in user,
-	 * and will add a link to the WordPress menu manager if logged in as an admin.
-	 *
-	 * @param array $args passed from the wp_nav_menu function.
-	 *
-	 */
-	public static function fallback($args) {
-		if (current_user_can('manage_options')) {
-
-			extract($args);
-
-			$fb_output = null;
-
-			if ($container) {
-				$fb_output = "<$container";
-
-				if ($container_id)
-					$fb_output .= " id=\"$container_id\"";
-
-				if ($container_class)
-					$fb_output .= " class=\"$container_class\"";
-
-				$fb_output .= '>';
-			}
-
-			$fb_output .= '<ul';
-
-			if ($menu_id)
-				$fb_output .= " id=\"$menu_id\"";
-
-			if ($menu_class)
-				$fb_output .= " class=\"$menu_class\"";
-
-			$fb_output .= '><li><a href="' . admin_url('nav-menus.php') . '">Add a menu</a></li></ul>';
-
-			if ($container)
-				$fb_output .= "</$container>";
-
-			echo $fb_output;
-		}
-	}
 }
 
 add_filter('wp_get_nav_menu_items','nav_items', 11, 3);
